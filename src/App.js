40366e5e6985cf2804a94273e6d670e1984db48d
Jayne Carmichael Norrie <39460834+jaynecn@users.noreply.github.com>
2019-06-13 23:38:08 +0100

    user items imported from github api array of 30 users

--- src/App.js
+++ src/App.js
@@ -1,38 +1,34 @@
-import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import React, { Component} from 'react';
 import Navbar from './components/layout/Navbar';
-import User from './components/users/User';
-import Alert from './components/layout/Alert';
-import Home from './components/pages/Home';
-import About from './components/pages/About';
-import NotFound from './components/pages/NotFound';
-
-import GithubState from './context/github/GithubState';
-import AlertState from './context/alert/AlertState';
-
+import Users from './components/users/Users';
 import './App.css';
+import axios from 'axios';
 
-const App = () => {
-  return (
-    <GithubState>
-      <AlertState>
-        <Router>
-          <div className='App'>
-            <Navbar />
-            <div className='container'>
-              <Alert />
-              <Switch>
-                <Route exact path='/' component={Home} />
-                <Route exact path='/about' component={About} />
-                <Route exact path='/user/:login' component={User} />
-                <Route component={NotFound} />
-              </Switch>
-            </div>
-          </div>
-        </Router>
-      </AlertState>
-    </GithubState>
-  );
-};
+class App extends Component{
+  state = {
+    users: [],
+    loading: false
+  }
+
+  async componentDidMount() {
+    this.setState({ loading: true});
+    const res = await axios.get('https://api.github.com/users');
+    
+    this.setState({ users: res.data, loading: false});
+  }
+
+  render() {
+    return (
+      <div className='App'>
+        <Navbar />
+        <div className="container">
+          <Users 
+          loading={this.state.loading} users={this.state.users}
+          />
+        </div>
+      </div>
+    );
+  }  
+}
 
 export default App;
